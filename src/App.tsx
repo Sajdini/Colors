1535f516e48df9785dfea37653decb2eb5bd9473
SamirAjdini <ajdinis1993@gmail.com>
2023-03-20 21:44:00 +0100

    Initial push

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">Something</div>;
+}
+
+export default App;
---
+++ src/components/InputSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InputSection = () => {
+  return <div>Input</div>;
+};
+
+export default InputSection;
